6f5eaa384d9468f5ec6be36ccfc51febf022b1b7
James Traver <jtraverjr@gmail.com>
2016-04-10 02:29:18 -0500

    idek

--- app/src/main/java/org/chiwardlocator/codeforcoffee/chicagowardlocator/MapsActivity.java
+++ app/src/main/java/org/chiwardlocator/codeforcoffee/chicagowardlocator/MapsActivity.java
@@ -10,28 +10,49 @@ import android.os.Bundle;
 import android.location.Location;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, OnLocationDetectionListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
     private Location location;
+    private LocationRequest mLocationRequest;
     private GoogleApiClient mGoogleApiClient;
     protected LocationManager locationManager;
     protected LocationListener locationListener;
     protected Context context;
     protected Double latitude, longitude;
 
-    @Override
-    public void onLocationDetected(Location mLocation) {
-        //Your new Location is received here
-        location = mLocation;
+    public void onConnected(Bundle connectionHint) throws SecurityException {
+        location = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (location != null) {
+            Double lat = location.getLatitude(),
+                    lon = location.getLongitude();
+            updateLatLong(lat, lon);
+            updateMapMarkerAndLocation();
+        }
+    }
+
+    protected void startLocationUpdates() {
+
+        // Define a listener that responds to location updates
+        //LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, nll);
     }
 
     private void updateLatLong(Double lat, Double lon) {
@@ -40,14 +61,45 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     protected void createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(10000);
         mLocationRequest.setFastestInterval(5000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
-    public void onConnected(Bundle connectionHint) {
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            Double lat = location.getLatitude(),
+                    lon = location.getLongitude();
+            updateLatLong(lat, lon);
+            updateMapMarkerAndLocation();
+        }
+    }
 
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
@@ -55,31 +107,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
         createLocationRequest();
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
+                        builder.build());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        // Acquire a reference to the system Location Manager
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
-        // Define a listener that responds to location updates
-        locationListener = new LocationListener() {
-            public void onLocationChanged(Location location) {
-                if (location != null) {
-                    Double lat = location.getLatitude(),
-                            lon = location.getLongitude();
-                    updateLatLong(lat, lon);
-                    updateMapMarkerAndLocation();
-                }
-            }
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
-            public void onProviderEnabled(String provider) {}
-            public void onProviderDisabled(String provider) {}
-        };
-
     }
 
     private void updateMapMarkerAndLocation() {
@@ -88,18 +137,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
     }
 
-    private void updateLocation() throws SecurityException {
-
-        Criteria criteria  = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        criteria.setAltitudeRequired(false);
-        criteria.setBearingRequired(false);
-        criteria.setCostAllowed(true);
-        criteria.setPowerRequirement(Criteria.POWER_LOW);
-
-        String bestProvider = locationManager.getBestProvider(criteria, true);
-        locationManager.requestLocationUpdates(bestProvider, 5000, 0, locationListener);
-    }
+//    private void updateLocation() throws SecurityException {
+//
+//        Criteria criteria  = new Criteria();
+//        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+//        criteria.setAltitudeRequired(false);
+//        criteria.setBearingRequired(false);
+//        criteria.setCostAllowed(true);
+//        criteria.setPowerRequirement(Criteria.POWER_LOW);
+//
+//        String bestProvider = locationManager.getBestProvider(criteria, true);
+//        locationManager.requestLocationUpdates(bestProvider, 5000, 0, locationListener);
+//    }
 
     /**
      * Manipulates the map once available.
@@ -114,14 +163,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
-        // Register the listener with the Location Manager to receive location updates
-        try {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 0, locationListener);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 0, locationListener);
-            updateLocation();
-        } catch (SecurityException securityExc) {
-            // let user know there is an error & log it
-        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
 }
