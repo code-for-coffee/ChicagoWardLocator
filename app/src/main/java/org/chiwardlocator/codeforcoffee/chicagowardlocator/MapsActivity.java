d2bfeec94cea7ab9d7c10a2a37e386158843c44e
James Traver <jtraverjr@gmail.com>
2016-04-10 16:16:07 -0500

    ok

--- app/src/main/java/org/chiwardlocator/codeforcoffee/chicagowardlocator/MapsActivity.java
+++ app/src/main/java/org/chiwardlocator/codeforcoffee/chicagowardlocator/MapsActivity.java
@@ -45,12 +45,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     final Double CHICAGO_LONG = -87.6500500;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     // sdk 23+ permissions (support for Android 6.0+ request
     int coarsePermissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION);
     int finePermissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
 
     protected static final int REQUEST_CHECK_SETTINGS = 100;
 =======
+=======
+>>>>>>> parent of d418992... adding api 23(marshmallow) permissions checks @ runtime
     protected static final int REQUEST_CHECK_SETTINGS = 0x1;
 >>>>>>> parent of d418992... adding api 23(marshmallow) permissions checks @ runtime
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
@@ -123,11 +126,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     @Override
     public void onLocationChanged(Location location) {
         handleLocationChange(location);
     }
 
+=======
+>>>>>>> parent of d418992... adding api 23(marshmallow) permissions checks @ runtime
 =======
 >>>>>>> parent of d418992... adding api 23(marshmallow) permissions checks @ runtime
     protected void createLocationRequest() {
