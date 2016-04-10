b3f23f73232eff1e1e28a9bbb8386f071e2754a1
James Traver <jtraverjr@gmail.com>
2016-04-10 00:02:47 -0500

    updated ward model

--- app/src/main/java/org/chiwardlocator/codeforcoffee/chicagowardlocator/WardModel.java
+++ app/src/main/java/org/chiwardlocator/codeforcoffee/chicagowardlocator/WardModel.java
@@ -11,8 +11,31 @@ import android.os.Bundle;
  */
 public class WardModel {
 
-    public WardModel() {
+    private String name;
+    private String alderman;
+    private String phoneNumber;
 
+    public WardModel(String name, String alderman, String phoneNumber) {
+        this.name = name;
+        this.alderman = alderman;
+        this.phoneNumber = phoneNumber;
+    }
+
+    private String convertStringToPrettyText(String text) {
+        String[] words = text.split(" ");
+        for(int inc = 0; inc <= words.length; inc++) {
+            words[inc] = words[inc].toLowerCase().substring(0,1).toUpperCase();
+        }
+        return new String(words.toString());
+    }
+
+    public String toString() {
+        String wardInformation = "";
+        wardInformation = wardInformation +
+                convertStringToPrettyText(this.name) + "\n " +
+                convertStringToPrettyText(this.alderman) + "\n" +
+                this.phoneNumber;
+        return wardInformation;
     }
 
 }
